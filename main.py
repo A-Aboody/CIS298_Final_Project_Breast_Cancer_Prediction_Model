117074465b268a72c63d36b185cada71ae577a23
Michael Luong <michalu@umich.edu>
2025-04-18 18:30:43 -0400

    add radar chart, using sidebar slider

--- main.py
+++ main.py
@@ -72,3 +72,73 @@ def configure_sidebar():
         )
 
     return user_input
+
+def create_radar_chart(normalized_input):
+    normalized_input = normalize_input(normalized_input)
+
+    measurement_categories = ['Radius', 'Texture', 'Perimeter', 'Area', 
+                              'Smoothness', 'Compactness', 
+                              'Concavity', 'Concave Points',
+                              'Symmetry', 'Fractal Dimension']
+
+    chart = go.Figure()
+
+    chart.add_trace(go.Scatterpolar(
+        r=[
+            normalized_input['radius_mean'], normalized_input['texture_mean'], normalized_input['perimeter_mean'],
+            normalized_input['area_mean'], normalized_input['smoothness_mean'], normalized_input['compactness_mean'],
+            normalized_input['concavity_mean'], normalized_input['concave points_mean'], normalized_input['symmetry_mean'],
+            normalized_input['fractal_dimension_mean']
+        ],
+        theta=measurement_categories,
+        fill='toself',
+        name='Mean Value'
+    ))
+
+    chart.add_trace(go.Scatterpolar(
+        r=[
+            normalized_input['radius_se'], normalized_input['texture_se'], normalized_input['perimeter_se'],
+            normalized_input['area_se'], normalized_input['smoothness_se'], normalized_input['compactness_se'],
+            normalized_input['concavity_se'], normalized_input['concave points_se'], normalized_input['symmetry_se'],
+            normalized_input['fractal_dimension_se']
+        ],
+        theta=measurement_categories,
+        fill='toself',
+        name='Standard Error'
+    ))
+
+    chart.add_trace(go.Scatterpolar(
+        r=[
+            normalized_input['radius_worst'], normalized_input['texture_worst'], normalized_input['perimeter_worst'],
+            normalized_input['area_worst'], normalized_input['smoothness_worst'], normalized_input['compactness_worst'],
+            normalized_input['concavity_worst'], normalized_input['concave points_worst'], normalized_input['symmetry_worst'],
+            normalized_input['fractal_dimension_worst']
+        ],
+        theta=measurement_categories,
+        fill='toself',
+        name='Worst Value'
+    ))
+
+    chart.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+                range=[0, 1]
+            )),
+        showlegend=True
+    )
+
+    return chart
+
+def main():
+    st.set_page_config(
+        page_title="Breast Cancer Prediction Model",
+    )
+
+    user_input = configure_sidebar()
+
+    with st.container():
+        st.title("Breast Cancer Prediction")
+        st.plotly_chart(create_radar_chart(user_input))
+
+main()
